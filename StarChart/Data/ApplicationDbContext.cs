567e5f7e6c05aa52ff442ca70b0c32d2e0a80838
Dennis Ying <dennisying@microsoft.com>
2021-07-12 15:29:24 +0800

    Fix namespace and typos.

--- StarChart/Data/ApplicationDbContext.cs
+++ StarChart/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using StarChart.Models;
 
 namespace StarChart.Data
 {
--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -3,21 +3,24 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-public class CelestialObject
+namespace StarChart.Models
 {
-	public int Id { get; set; }
-
-	[Required]
-	public string Name { get; set; }
-
-	public int? OrbitedObjectID { get; set; }
-
-	[NotMapped]
-    public List<CelestialObject> Satellites { get; set; }
-
-    public TimeSpan OrbitalPeriod { get; set; }
-
-    public CelestialObject()
+	public class CelestialObject
 	{
+		public int Id { get; set; }
+
+		[Required]
+		public string Name { get; set; }
+
+		public int? OrbitedObjectId { get; set; }
+
+		[NotMapped]
+		public List<CelestialObject> Satellites { get; set; }
+
+		public TimeSpan OrbitalPeriod { get; set; }
+
+		public CelestialObject()
+		{
+		}
 	}
 }
